6a0c631dd4c5a6b51e812f4b922dd55efa9dbfc7
iwokrzysztoporski-max <iwokrzysztoporski@gmail.com>
2026-06-27 00:14:38 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Klucz "anon public" JEST publiczny z założenia — może tu być, to bezpieczne.
 // NIE wklejaj tu klucza "service_role".
 
-window.SUPABASE_URL = "https://ykosbmchhoduaguxbgoz.supabase.co/rest/v1/";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrb3NibWNoaG9kdWFndXhiZ296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MDM1MDYsImV4cCI6MjA5ODA3OTUwNn0.ZLX_0XU_Q1EJK-gXWTW8T0EvUaINmtSHvDZYIrFxmns;
+window.SUPABASE_URL = "https://ykosbmchhoduaguxbgoz.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrb3NibWNoaG9kdWFndXhiZ296Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MDM1MDYsImV4cCI6MjA5ODA3OTUwNn0.ZLX_0XU_Q1EJK-gXWTW8T0EvUaINmtSHvDZYIrFxmns";
